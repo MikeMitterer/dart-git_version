e580779f0f1edf7887a315114c98ad2f87146665
Mike Mitterer <office@mikemitterer.at>
2017-11-30 11:13:01 +0100

    Jenkins integration

---
+++ tool/grind.dart
@@ -0,0 +1,43 @@
+import 'package:grinder/grinder.dart';
+
+main(final List<String> args) {
+    args.forEach((final String arg) => print(arg));
+    grind(args);
+}
+
+@Task()
+@Depends(test)
+build() {
+}
+
+@Task()
+clean() => defaultClean();
+
+@Task()
+@Depends(analyze)
+test() {
+    //new TestRunner().testAsync(files: "test/unit");
+    new PubApp.local('test').run([]);
+    // new TestRunner().testAsync(files: "test/integration");
+
+    // Alle test mit @TestOn("content-shell") im header
+    // new TestRunner().test(files: "test/unit",platformSelector: "content-shell");
+    // new TestRunner().test(files: "test/integration",platformSelector: "content-shell");
+}
+
+@Task()
+analyze() {
+    final List<String> libs = [
+        "lib/git_version.dart",
+        "bin/showGitVersion.dart"
+    ];
+
+    libs.forEach((final String lib) => Analyzer.analyze(lib));
+    // Analyzer.analyze("test");
+}
+
+@Task('Deploy built app.')
+//@Depends(build, test)
+deploy() {
+    new PubApp.global('git_version').activate();
+}
